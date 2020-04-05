99da604c89c776db2d629b50258b16be269d977d
Jie Lan <lanjie3458@gmail.com>
2020-04-05 13:56:30 -0400

    add 400 error code

--- src/main/java/springframework/guru/repoSearchEngine/controller/RepoController.java
+++ src/main/java/springframework/guru/repoSearchEngine/controller/RepoController.java
@@ -1,6 +1,7 @@
 package springframework.guru.repoSearchEngine.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.server.ServerWebInputException;
 import springframework.guru.repoSearchEngine.dto.models.RepoDetail;
 import springframework.guru.repoSearchEngine.dto.models.RepoSearchItem;
 import springframework.guru.repoSearchEngine.exception.ErrorMsg;
@@ -12,7 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 import javax.validation.constraints.Pattern;
 import java.util.ArrayList;
@@ -33,7 +34,9 @@ public class RepoController {
     }
 
     @GetMapping("/search")
-    public ResponseEntity<ArrayList<RepoSearchItem>> searchRepos(@RequestParam String searchKey) {
+    public ResponseEntity<ArrayList<RepoSearchItem>> searchRepos(
+            @Valid @Pattern(regexp = "(.|\\s)*\\S(.|\\s)*",message = "Invalid SearchKey Param Value")
+            @RequestParam String searchKey) {
         try{
             ArrayList<RepoSearchItem> results = repoSearchService.searchRepo(searchKey);
             return new ResponseEntity<>(results, HttpStatus.OK);
@@ -45,8 +48,9 @@ public class RepoController {
 
     @GetMapping("/detail")
     public ResponseEntity<RepoDetail> getSingleRepo(
-            @Valid @Pattern(regexp = "^(github|gitlab|bitbucket)$",message = "Platform Param Value Error")
+            @Valid @Pattern(regexp = "^(github|gitlab|bitbucket)$",message = "Invalid Platform Param Value")
             @RequestParam String platform,
+            @Valid @Pattern(regexp = "(.|\\s)*\\S(.|\\s)*",message = "Invalid Full_name Param Value")
             @RequestParam String full_name) {
         try{
             RepoDetail repoDetail= repoDetailService.acquireRepoDetail(platform, full_name);
@@ -69,7 +73,12 @@ public class RepoController {
     public ResponseEntity<ErrorMsg> handleError(Exception ex) {
         logger.error("Error: - Status {}, Body {}", ex.getMessage(),
                 ex.getMessage());
-        ErrorMsg responseError = new ErrorMsg(ex);
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseError);
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        ErrorMsg responseError = new ErrorMsg(ex, status);
+        if(ex instanceof ConstraintViolationException || ex instanceof ServerWebInputException){
+            status = HttpStatus.BAD_REQUEST;
+            responseError.setStatus(status);
+        }
+        return ResponseEntity.status(status).body(responseError);
     }
 }

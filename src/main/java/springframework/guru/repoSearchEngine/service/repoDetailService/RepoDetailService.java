4915ff5cb0641eab825284f6196ef1de72179b7b
Jie Lan <lanjie3458@gmail.com>
2020-04-12 13:09:17 -0400

    get commits for past 2 years

--- src/main/java/springframework/guru/repoSearchEngine/service/repoDetailService/RepoDetailService.java
+++ src/main/java/springframework/guru/repoSearchEngine/service/repoDetailService/RepoDetailService.java
@@ -2,13 +2,16 @@ package springframework.guru.repoSearchEngine.service.repoDetailService;
 
 import springframework.guru.repoSearchEngine.dto.response.RepoDetail;
 import java.util.ArrayList;
+import java.util.List;
 
 public interface RepoDetailService {
     RepoDetail acquireRepoDetail(String platform, String full_name);
     RepoDetail acquireRepoMeta(String platform, String full_name);
-    ArrayList<String> acquireRepoCommits(String platform, String full_name);
-    Boolean checkCommits(String platform, ArrayList<String> commits_total,
+    ArrayList<String> acquireRepoCommits(String platform, String full_name) ;
+    Boolean checkCommitsSize(String platform, ArrayList<String> commits_total,
                             ArrayList<String> commits_single_page);
+    void checkCommitsDate(List<String> commits_single_page);
+    Boolean checkDate(String date) throws Exception;
 }
 
 
--- src/main/java/springframework/guru/repoSearchEngine/service/repoDetailService/RepoDetailServiceImpl.java
+++ src/main/java/springframework/guru/repoSearchEngine/service/repoDetailService/RepoDetailServiceImpl.java
@@ -1,4 +1,5 @@
 package springframework.guru.repoSearchEngine.service.repoDetailService;
+
 import org.springframework.stereotype.Service;
 import springframework.guru.repoSearchEngine.dto.response.RepoDetail;
 import springframework.guru.repoSearchEngine.dto.bitbucket.BitbucketRepoDto;
@@ -7,7 +8,12 @@ import springframework.guru.repoSearchEngine.dto.gitlab.GitlabRepoDto;
 import springframework.guru.repoSearchEngine.service.bitbucketApiService.BitbucketApiService;
 import springframework.guru.repoSearchEngine.service.githubApiService.GithubApiService;
 import springframework.guru.repoSearchEngine.service.gitlabApiService.GitlabApiService;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class RepoDetailServiceImpl implements RepoDetailService{
@@ -59,13 +65,11 @@ public class RepoDetailServiceImpl implements RepoDetailService{
         }
     }
 
-    //acquire the first 300 latest Commits
     @Override
-    public ArrayList<String>  acquireRepoCommits(String platform, String full_name){
+    public ArrayList<String>  acquireRepoCommits(String platform, String full_name) {
         try{
             ArrayList<String> commits_total = new ArrayList<>();
             int current_page = 1;
-
             while(true){
                 ArrayList<String> commits_single_page =new ArrayList<>();
                 if(platform.equals("bitbucket"))
@@ -74,8 +78,10 @@ public class RepoDetailServiceImpl implements RepoDetailService{
                     commits_single_page = gitlabApiService.getRepoCommits(full_name, current_page++);
                 else if(platform.equals("github"))
                     commits_single_page = githubAPIService.getRepoCommits(full_name,current_page++);
+
+                checkCommitsDate(commits_single_page);
                 commits_total.addAll(commits_single_page);
-                if(!checkCommits(platform, commits_total, commits_single_page))
+                if(!checkCommitsSize(platform, commits_total, commits_single_page))
                     break;
             }
             return commits_total;
@@ -86,19 +92,61 @@ public class RepoDetailServiceImpl implements RepoDetailService{
     }
 
     @Override
-    public Boolean checkCommits(String platform, ArrayList<String> commits_total,
+    public Boolean checkCommitsSize(String platform, ArrayList<String> commits_total,
                                 ArrayList<String> commits_single_page){
         try{
             int MAX_COMMITS_PER_PAGE = platform.equals("gitlab")? 20 : 30;
-            if(commits_total.size() > 300 - MAX_COMMITS_PER_PAGE)
+            if(commits_total.size() +  MAX_COMMITS_PER_PAGE > 300)
                 return false;
             else if(commits_single_page.size() < MAX_COMMITS_PER_PAGE)
                 return false;
             else
                 return true;
         } catch(Exception ex){
+
             throw ex;
         }
+    }
 
+    @Override
+    public void checkCommitsDate(List<String> commits_single_page){
+        String last_date = commits_single_page.get(0);
+        if(!checkDate(last_date)){
+            commits_single_page.clear();
+            return;
+        }
+
+        String first_date = commits_single_page.get(commits_single_page.size() - 1);
+        if(checkDate(first_date))
+            return;
+
+        for (int i = 0; i < commits_single_page.size(); i++) {
+            String tempDate = commits_single_page.get(i);
+            if(checkDate(tempDate))
+                continue;
+            else {
+                List<String> temp_commits =  commits_single_page.subList(i, commits_single_page.size());
+                temp_commits.clear();
+                break;
+            }
+        }
+    }
+
+    @Override
+    public Boolean checkDate(String date) {
+        try{
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+            Date startDate = formatter.parse(date.substring(0,10));
+            Date endDate = new Date();
+            long diffIn_millies = Math.abs(endDate.getTime() - startDate.getTime());
+            long diff_dates = TimeUnit.DAYS.convert(diffIn_millies, TimeUnit.MILLISECONDS);
+            if (diff_dates > 365 * 2)
+                return false;
+            return true;
+        } catch(java.text.ParseException ex){
+            return false;
+        } catch(Exception ex){
+            throw ex;
+        }
     }
 }
